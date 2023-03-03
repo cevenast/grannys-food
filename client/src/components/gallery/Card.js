91026474d7b4b66ba86cf1c0502463768abcbc90
Cristobal Evensen <cevensen.a@gmail.com>
2023-03-03 00:29:57 -0300

    Connected react to server to fetch recipes with axios

--- client/src/components/gallery/Card.js
+++ client/src/components/gallery/Card.js
@@ -1,6 +1,6 @@
 import Tag from './Tag'
 
-export default function Card({title, user, tags, imgSrc, id, description}){
+export default function Card({title, userName, userId, tags, imgSrc, id, description}){
     const tagList = tags.map((tag, index) => <Tag tag={tag} key={index}/>)
 
 
@@ -9,7 +9,7 @@ export default function Card({title, user, tags, imgSrc, id, description}){
 
             <a href={`/recipes/${id}`}><img src={imgSrc} alt={title} className="w-56 h-36"/></a>
             <h3 className="w-full text-2xl pt-2"><a href={`/recipes/${id}`} className=" hover:text-[#4a7b8a]">{title}</a></h3>
-            <span className="align-top text-xs text-zinc-400"><a className="hover:text-[#cdbbb2]" href="#">{`@${user}`}</a></span>
+            <span className="align-top text-xs text-zinc-400"><a className="hover:text-[#cdbbb2]" href={`user/${userId}`}>{`@${userName}`}</a></span>
             <p className="text-xs pt-4">{description}</p>
             <div className="flex flex-row flex-wrap justify-start pt-4">
                 {tagList}
