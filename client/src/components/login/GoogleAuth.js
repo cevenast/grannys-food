b22917f50ca7bec4e7c9e66c51cbdbc85651867b
Cristobal Evensen <cevensen.a@gmail.com>
2023-04-11 17:38:03 -0400

    Added message warning that google auth functionality still in development

--- client/src/components/login/GoogleAuth.js
+++ client/src/components/login/GoogleAuth.js
@@ -1,14 +1,30 @@
-
+import { useState } from 'react'
 
 export default function GoogleAuth(props){
+  const [showMessage, setShowMessage] = useState(false)
+
+  function handleClick(){
+    setShowMessage(true)
+    setShowMessage('Google Auth functionality still in development')
+    setTimeout(() => setShowMessage(false), 1500)
+  }
+
   return(
     <>
     
-    <a href="/auth/google">
-    <div className="h-11 w-full px-4 py-2 mt-12 text-center bg-slate-200 hover:bg-slate-300 duration-200 ">  
-      <span className="text-sm pl-8 align-middle font-bold text-slate-700 bg-[url('/bg-google.svg')] bg-no-repeat bg-left bg-contain ">Continue with Google</span>
-    </div>
-    </a>
+    <button className='w-full' onClick={handleClick}>
+      <div className="h-11 w-full px-4 py-2 mt-12 text-center bg-slate-200 hover:bg-slate-300 duration-200 ">  
+        <span className="text-sm pl-8 align-middle font-bold text-slate-700 bg-[url('/bg-google.svg')] bg-no-repeat bg-left bg-contain ">
+          Continue with Google
+        </span>
+      </div>
+    </button>
+
+    {showMessage &&
+                  <div className="w-screen h-screen bg-slate-100/50 fixed top-0 left-0 flex flex-column justify-center items-center">
+                   <p className="text-xl ">{showMessage}</p>
+                  </div>
+    }
 
     </>
   )
